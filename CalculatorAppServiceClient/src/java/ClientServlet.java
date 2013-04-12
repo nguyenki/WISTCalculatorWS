77b850e4d5db4bd9ab0d3226f3201bbb8c7d98f6
Kim Thuat NGUYEN <nguyenkimthuat@gmail.com>
2013-04-12 18:03:52 +0200

    add simple webservice client

---
+++ CalculatorAppServiceClient/src/java/ClientServlet.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.nguyenki.calculatorws.CalculatorWS_Service;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+
+/**
+ *
+ * @author nguyenkimthuat
+ */
+@WebServlet(urlPatterns = {"/ClientServlet"})
+public class ClientServlet extends HttpServlet {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/CalculatorApp/CalculatorWS.wsdl")
+    private CalculatorWS_Service service;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ClientServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ClientServlet at " + request.getContextPath() + "</h1>");
+            
+            int i = 23;
+            int j= 40;
+            com.nguyenki.calculatorws.CalculatorWS port = service.getCalculatorWSPort();
+           
+            int result = port.add(i, j);
+            out.println("<h2>Result of "+i+"+"+j+"  is "+result+"</h2>");
+            
+          
+            
+            out.println("</body>");
+            out.println("</html>");
+        }   catch (Exception ex) {
+             out.println("<p>Exception: " + ex);
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private Integer add(int i, int j) {
+        com.nguyenki.calculatorws.CalculatorWS port = service.getCalculatorWSPort();
+        return port.add(i, j);
+    }
+}
